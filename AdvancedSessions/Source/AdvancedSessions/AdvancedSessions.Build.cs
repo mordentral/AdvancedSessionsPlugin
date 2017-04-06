4388f57f3db3dec29aacba3604c8728a347fef08
mordentral <morden@noxhub.net>
2017-04-06 12:11:16 -0400

    added preprocessor definitions to the plugin so c++ can control different code paths depending on if they are enabled or not.
    Former-commit-id: 42f000697cc3095d2795bfc3b9a2a79db94af741

--- AdvancedSessions/Source/AdvancedSessions/AdvancedSessions.Build.cs
+++ AdvancedSessions/Source/AdvancedSessions/AdvancedSessions.Build.cs
@@ -5,6 +5,8 @@ public class AdvancedSessions : ModuleRules
 {
     public AdvancedSessions(TargetInfo Target)
     {
+        Definitions.Add("WITH_ADVANCED_SESSIONS=1");
+
         PrivateIncludePaths.AddRange(new string[] { "AdvancedSessions/Private"/*, "OnlineSubsystemSteam/Private"*/ });
         PublicIncludePaths.AddRange(new string[] { "AdvancedSessions/Public" });
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "CoreUObject", "OnlineSubsystemUtils", "Networking", "Sockets"/*"Voice", "OnlineSubsystemSteam"*/ });
--- AdvancedSteamSessions/Source/AdvancedSteamSessions/AdvancedSteamSessions.Build.cs
+++ AdvancedSteamSessions/Source/AdvancedSteamSessions/AdvancedSteamSessions.Build.cs
@@ -4,8 +4,10 @@ using System.IO;
 public class AdvancedSteamSessions : ModuleRules
 {
     public AdvancedSteamSessions(TargetInfo Target)
-    {  
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "CoreUObject", "OnlineSubsystemUtils", "Networking", "Sockets", "AdvancedSessions"/*"Voice", "OnlineSubsystemSteam"*/ });
+    {
+        Definitions.Add("WITH_ADVANCED_STEAM_SESSIONS=1");
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "CoreUObject", "OnlineSubsystemUtils", "Networking", "Sockets", "AdvancedSessions"/*"Voice", "OnlineSubsystemSteam"*/ });
         PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "Sockets", "Networking", "OnlineSubsystemUtils" /*"Voice", "Steamworks","OnlineSubsystemSteam"*/});
 
         if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Linux) || (Target.Platform == UnrealTargetPlatform.Mac))
