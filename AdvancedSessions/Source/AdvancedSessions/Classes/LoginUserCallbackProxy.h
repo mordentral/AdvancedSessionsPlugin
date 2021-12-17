444b632a725ac5b440f1f60809f314c77cd8c175
Ji-Rath <garethaguilar@gmail.com>
2021-12-17 15:27:22 -0600

    Add type parameter to LoginUser
    Add type parameter so EOS can use "accountportal" to authenticate, falls back to default implementation if left blank

--- AdvancedSessions/Source/AdvancedSessions/Classes/LoginUserCallbackProxy.h
+++ AdvancedSessions/Source/AdvancedSessions/Classes/LoginUserCallbackProxy.h
@@ -21,8 +21,8 @@ class ULoginUserCallbackProxy : public UOnlineBlueprintCallProxyBase
 	FEmptyOnlineDelegate OnFailure;
 
 	// Logs into the identity interface
-	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly = "true", WorldContext="WorldContextObject"), Category = "Online|AdvancedIdentity")
-	static ULoginUserCallbackProxy* LoginUser(UObject* WorldContextObject, class APlayerController* PlayerController, FString UserID, FString UserToken);
+	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly = "true", WorldContext="WorldContextObject", AdvancedDisplay = "Type"), Category = "Online|AdvancedIdentity")
+	static ULoginUserCallbackProxy* LoginUser(UObject* WorldContextObject, class APlayerController* PlayerController, FString UserID, FString UserToken, FString Type);
 
 	// UOnlineBlueprintCallProxyBase interface
 	virtual void Activate() override;
@@ -41,6 +41,8 @@ private:
 
 	// The user pass / token
 	FString UserToken;
+
+	FString Type;
 
 	// The delegate executed by the online subsystem
 	FOnLoginCompleteDelegate Delegate;
