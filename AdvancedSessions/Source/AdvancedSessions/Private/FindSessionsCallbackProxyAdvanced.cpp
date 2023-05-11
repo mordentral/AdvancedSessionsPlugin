b18041438ed5d308d403c2393bbf6d00c70db8a6
Joshua <mordentral@gmail.com>
2023-05-11 15:13:15 -0400

    online session online session names additionnames addition

--- AdvancedSessions/Source/AdvancedSessions/Private/FindSessionsCallbackProxyAdvanced.cpp
+++ AdvancedSessions/Source/AdvancedSessions/Private/FindSessionsCallbackProxyAdvanced.cpp
@@ -1,6 +1,7 @@
 // Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
 #include "FindSessionsCallbackProxyAdvanced.h"
 
+#include "Online/OnlineSessionNames.h"
 
 //////////////////////////////////////////////////////////////////////////
 // UFindSessionsCallbackProxyAdvanced
