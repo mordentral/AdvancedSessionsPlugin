141b15c4e4fb65ad233df52400c9b92f124da4c7
Joshua <mordentral@gmail.com>
2023-07-26 08:53:01 -0400

    update session add advertisement

--- AdvancedSessions/Source/AdvancedSessions/Classes/UpdateSessionCallbackProxyAdvanced.h
+++ AdvancedSessions/Source/AdvancedSessions/Classes/UpdateSessionCallbackProxyAdvanced.h
@@ -21,7 +21,7 @@ class UUpdateSessionCallbackProxyAdvanced : public UOnlineBlueprintCallProxyBase
 
 	// Creates a session with the default online subsystem with advanced optional inputs, you MUST fill in all categories or it will pass in values that you didn't want as default values
 	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly = "true", WorldContext="WorldContextObject",AutoCreateRefTerm="ExtraSettings"), Category = "Online|AdvancedSessions")
-	static UUpdateSessionCallbackProxyAdvanced* UpdateSession(UObject* WorldContextObject, const TArray<FSessionPropertyKeyPair> &ExtraSettings, int32 PublicConnections = 100, int32 PrivateConnections = 0, bool bUseLAN = false, bool bAllowInvites = false, bool bAllowJoinInProgress = false, bool bRefreshOnlineData = true, bool bIsDedicatedServer = false);
+	static UUpdateSessionCallbackProxyAdvanced* UpdateSession(UObject* WorldContextObject, const TArray<FSessionPropertyKeyPair> &ExtraSettings, int32 PublicConnections = 100, int32 PrivateConnections = 0, bool bUseLAN = false, bool bAllowInvites = false, bool bAllowJoinInProgress = false, bool bRefreshOnlineData = true, bool bIsDedicatedServer = false, bool bShouldAdvertise = true);
 
 	// UOnlineBlueprintCallProxyBase interface
 	virtual void Activate() override;
@@ -38,28 +38,30 @@ private:
 	FDelegateHandle OnUpdateSessionCompleteDelegateHandle;
 
 	// Number of public connections
-	int NumPublicConnections;
+	int NumPublicConnections = 100;
 
 	// Number of private connections
-	int NumPrivateConnections;
+	int NumPrivateConnections = 0;
 
 	// Whether or not to search LAN
-	bool bUseLAN;
+	bool bUseLAN = false;
 
 	// Whether or not to allow invites
-	bool bAllowInvites;
+	bool bAllowInvites = true;
 
 	// Store extra settings
 	TArray<FSessionPropertyKeyPair> ExtraSettings;
 
 	// Whether to update the online data
-	bool bRefreshOnlineData;
+	bool bRefreshOnlineData = true;
 
 	// Allow joining in progress
-	bool bAllowJoinInProgress;
+	bool bAllowJoinInProgress = true;
 
 	// Update whether this is a dedicated server or not
-	bool bDedicatedServer;
+	bool bDedicatedServer = false;
+
+	bool bShouldAdvertise = true;
 
 	// The world context object in which this call is taking place
 	UObject* WorldContextObject;
