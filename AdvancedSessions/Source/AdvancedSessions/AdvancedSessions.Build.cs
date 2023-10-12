9713ca512c475ceefa81c1cea42c9d5703ff41df
Joshua <mordentral@gmail.com>
2023-10-12 11:06:47 -0400

    not sure why these were both in private and public dependencies, looks like a copy paste typo.

--- AdvancedSessions/Source/AdvancedSessions/AdvancedSessions.Build.cs
+++ AdvancedSessions/Source/AdvancedSessions/AdvancedSessions.Build.cs
@@ -13,6 +13,5 @@ public class AdvancedSessions : ModuleRules
        // PrivateIncludePaths.AddRange(new string[] { "AdvancedSessions/Private"/*, "OnlineSubsystemSteam/Private"*/ });
        // PublicIncludePaths.AddRange(new string[] { "AdvancedSessions/Public" });
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "CoreUObject", "OnlineSubsystemUtils", "Networking", "Sockets"/*"Voice", "OnlineSubsystemSteam"*/ });
-        PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "Sockets", "Networking", "OnlineSubsystemUtils" /*"Voice", "Steamworks","OnlineSubsystemSteam"*/});
     }
 }
