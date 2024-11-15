711cb071219b5eb2a7eeccdd146872aa5066dcc2
Joshua <mordentral@gmail.com>
2024-11-15 08:54:29 -0500

    move loading phase later and directly link to steam sdk

--- AdvancedSteamSessions/Source/AdvancedSteamSessions/AdvancedSteamSessions.Build.cs
+++ AdvancedSteamSessions/Source/AdvancedSteamSessions/AdvancedSteamSessions.Build.cs
@@ -16,6 +16,7 @@ public class AdvancedSteamSessions : ModuleRules
         if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Linux) || (Target.Platform == UnrealTargetPlatform.Mac))
         {
             PublicDependencyModuleNames.AddRange(new string[] { "SteamShared", "Steamworks", "OnlineSubsystemSteam" });
+            AddEngineThirdPartyPrivateStaticDependencies(Target, "Steamworks");
             //PublicIncludePaths.AddRange(new string[] { "../Plugins/Online/OnlineSubsystemSteam/Source/Private" });// This is dumb but it isn't very open
         }
     }
