e8f3ffd1be6ff974bb2b67fe741418056b9c34dc
Joshua <mordentral@gmail.com>
2024-11-19 12:19:23 -0500

    Adding missing includes to steam friends library texture composition

--- AdvancedSteamSessions/Source/AdvancedSteamSessions/Private/AdvancedSteamFriendsLibrary.cpp
+++ AdvancedSteamSessions/Source/AdvancedSteamSessions/Private/AdvancedSteamFriendsLibrary.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "AdvancedSteamFriendsLibrary.h"
 #include "OnlineSubSystemHeader.h"
+#include "Engine/Texture.h"
+#include "Engine/Texture2D.h"
+#include "PixelFormat.h"
 
 //General Log
 DEFINE_LOG_CATEGORY(AdvancedSteamFriendsLog);
